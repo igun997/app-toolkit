5fd589cd96f2403da8c4aa0c28fdcca129f27e83
Indra <indra@imceria.com>
2020-09-18 15:39:53 +0700

    Auto Schedule

---
+++ src/Objects/AutoScedulingResponse.php
@@ -0,0 +1,26 @@
+<?php
+
+class AutoScedulingResponse {
+    public array $response;
+    public string $date;
+
+    protected $fields = [
+        "response",
+        "date"
+    ];
+
+    public function __construct(array $response)
+    {
+        $this->response = $response["data"];
+        $this->date = $response["date"];
+    }
+
+    public function toArray()
+    {
+        $resp = [];
+        foreach ($this->fields as $index => $field) {
+            $resp[$field] = $this->$field;
+        }
+        return $resp;
+    }
+}
---
+++ src/Objects/Entity.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace igun997\Objects;
+/***
+ * Class Entity
+ * @package igun997\Objects
+ */
+class Entity {
+    public $id;
+    public String $name;
+    public int $division_id;
+    public int $hours;
+    public ?array $schedule;
+
+    protected $fields = [
+        "id",
+        "name",
+        "division_id",
+        "hours",
+        "schedule"
+    ];
+
+    /***
+     * Entity constructor.
+     * @param Object $data
+     */
+    public function __construct(Object $data)
+    {
+        $this->id = $data->id;
+        $this->name = $data->name;
+        $this->hours = $data->hours;
+        $this->division_id = $data->division_id;
+        $this->schedule = ((isset($data->schedule))?$data->schedule:NULL);
+    }
+
+    /***
+     * @return array
+     */
+    public function toArray()
+    {
+        $response = [];
+        foreach ($this->fields as $index => $field) {
+            $response[$field] = $this->$field;
+        }
+        return $response;
+    }
+}
